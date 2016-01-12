6cf740b6dbd81b5ff6b17a7d85452b3a362efb1e
seb <sebast33500@gmail.com>
2016-01-12 16:17:55 +0100

    Initial commit formation.html   promotion.html accessibilite and Mustache.js

---
+++ simplonMip/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+$.getJSON("user.json", function(data){
+	var tpl = $('#cardTemplate').html();
+	var html = Mustache.render(tpl,data);
+	$('#container').html(html);
+
+});
+});
